f9e4cdae6df8c3166d2e18da4f23c991990a9dd4
Pedro Casas Martínez <pedrocasas876@gmail.com>
2020-12-23 16:07:33 +0100

    proyecto creado y modelos incluidos

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static float musicVolume = 0.5f;
+    public static float effectsVolume = 0.5f;
+}
