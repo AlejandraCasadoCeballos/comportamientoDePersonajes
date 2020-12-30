4cb24776ac49030319d2f5aaa8a858f00bded534
Pedro Casas Martínez <pedrocasas876@gmail.com>
2020-12-30 10:37:26 +0100

    scripts nodos de acciones y percepciones

---
+++ Assets/Scripts/AI/ActionNode.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ActionNode : Node
+{
+    public enum Reevaluation { onlyOnEnd, atFixedRate};
+
+    Action onBegin;
+    Action onUpdate;
+    Action onEnd;
+
+    public float reevaluationRate;
+    public Reevaluation reevaluationMode;
+
+    public ActionNode(float reevaluationRate = 1f, Reevaluation reevaluationMode = Reevaluation.onlyOnEnd)
+    {
+        this.reevaluationRate = reevaluationRate;
+        this.reevaluationMode = reevaluationMode;
+    }
+
+    public void Begin()
+    {
+        onBegin?.Invoke();
+    }
+
+    public void Update()
+    {
+        onUpdate?.Invoke();
+    }
+
+    public void End()
+    {
+        onEnd?.Invoke();
+    }
+
+    public ActionNode SetOnBegin(Action action)
+    {
+        onBegin = action;
+        return this;
+    }
+
+    public ActionNode SetOnUpdate(Action action)
+    {
+        onUpdate = action;
+        return this;
+    }
+
+    public ActionNode SetOnEnd(Action action)
+    {
+        onEnd = action;
+        return this;
+    }
+}
---
+++ Assets/Scripts/AI/PerceptionNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PerceptionNode : Node
+{
+    Func<bool> evaluationFunc;
+
+    public PerceptionNode(Func<bool> func)
+    {
+        evaluationFunc = func;
+    }
+
+    override public bool Evaluate()
+    {
+        return evaluationFunc != null ? evaluationFunc() : true;
+    }
+}
