7160cfedeaef64ba527806c09233a8ee4b978d9d
Juanma <juanmcasper5@gmail.com>
2020-12-30 19:16:23 +0100

    FSM sin la parte de end only

---
+++ Assets/Scripts/AI/FSM_Edge.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FSM_Edge : MonoBehaviour
+{
+    protected FSM_Node src_node;
+    public FSM_Node dst_node;
+    protected List<Func<bool>> conditions; 
+
+   public FSM_Edge(FSM_Node nodeA, FSM_Node nodeB, List<Func<bool>> nodeCondition)
+   {
+        this.src_node = nodeA;
+        this.dst_node = nodeB;
+        this.conditions = nodeCondition;
+   }
+
+   public void AddCondition(Func<bool> condition)
+   {
+        conditions.Add(condition);
+   }
+    
+    public bool CheckConditions()
+    {
+        foreach (var condition in conditions)
+        {
+            if (!condition())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Assets/Scripts/AI/FSM_Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM_Node : ActionNode
+{
+    public List<FSM_Edge> myEdges;
+
+    public FSM_Node(FSM_Edge[] Edges) : base()
+    {
+        this.myEdges = Edges != null ? new List<FSM_Edge>(Edges) : new List<FSM_Edge>(); //Si no me equivoco aquí puedo crear un nodo que necesariamente no tenga aristas
+    }
+
+    public void AddEdge(FSM_Edge edge)
+    {
+        myEdges.Add(edge);
+    }
+
+
+}
