ba2b60826949414c36594c904633bf83ef40306f
Juanma <juanmcasper5@gmail.com>
2021-01-03 22:33:09 +0100

    Sonidos

---
+++ Assets/Scripts/AI/AudioScripts/AudioManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource baseCapturandose;
+
+    [SerializeField]
+    private AudioSource baseCapturada;
+
+    public BaseBehaviour baseComponent;
+
+    private int numBases;
+    private void Start()
+    {
+        numBases  = BaseBehaviour.bases.Count();
+
+
+    }
+
+    void Update()
+    {
+        if (baseComponent.isBeingConquered)
+        {
+            baseCapturandose.Play();
+        }
+        else
+        {
+            if (baseCapturandose.isPlaying)
+            {
+                baseCapturandose.Stop();
+            }
+        }
+
+        foreach (var equipo in BaseBehaviour.bases)
+        {
+            if(equipo.team > 0)
+            {
+
+            }
+        }
+    }
+}
